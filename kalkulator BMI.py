a46bd83cf5b11b2cfcfceb6867c4772a9ef7f296
BigMagicArcher <118203245+BigMagicArcher@users.noreply.github.com>
2024-12-26 18:47:37 +0100

    Working BMI calc

--- kalkulator BMI.py
+++ kalkulator BMI.py
@@ -1,14 +1,28 @@
-import re 
+import time
+import re
+print("Witaj w kalkulatorze BMI (Body Mass Index)")
+print("Nie dotyczy to dzieci!")
+time.sleep(2)
 while True:
-    zadanie=input("Podaj działanie matematyczne lub wpisz exit aby wyjść: ")
-    if zadanie == "exit":
+    waga=input("Podaj swoją wagę w kilogramach lub wpisz exit aby wyjść: ")
+    wzrost=input("Podaj swój wzrost w centymetrach: ")
+    if waga == "exit" or wzrost == "exit":
         print("Do widzenia")
         break
-    sprawdzenie = re.match(r"[a-z]", zadanie)
-    
-    if sprawdzenie is None:
-        print("{} = {}".format(zadanie, eval(zadanie)))
+    sprawdzenie = re.match(r"[a-z]", waga)
+    sprawdzenie2 = re.match(r"[a-z]", wzrost)
+    if sprawdzenie is None or sprawdzenie2 is None:
+        int_waga = int(waga)
+        int_wzrost = int(wzrost)
+        int_wzrost = int_wzrost/100
+        wynik = round(int_waga/int_wzrost**2, 1)
+        print("Twoje BMI to {}".format(wynik))
+        if wynik >= 50:
+            print("Musisz zastosować dietę!")
+        if wynik <= 10:
+            print("Nawet mój ojciec nie jest taki chudy!")           
     else:
-        print("Żle napisane działanie!")
+        print("Źle podana wartość!")
+    print()
     
          
