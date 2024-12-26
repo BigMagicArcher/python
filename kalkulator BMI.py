643db1d8f498848187711b8618eab709cdf0e60b
BigMagicArcher <118203245+BigMagicArcher@users.noreply.github.com>
2024-12-26 19:10:14 +0100

    ValueError obsłużony wyjątek

--- kalkulator BMI.py
+++ kalkulator BMI.py
@@ -4,25 +4,31 @@ print("Witaj w kalkulatorze BMI (Body Mass Index)")
 print("Nie dotyczy to dzieci!")
 time.sleep(2)
 while True:
-    waga=input("Podaj swoją wagę w kilogramach lub wpisz exit aby wyjść: ")
-    wzrost=input("Podaj swój wzrost w centymetrach: ")
-    if waga == "exit" or wzrost == "exit":
-        print("Do widzenia")
-        break
-    sprawdzenie = re.match(r"[a-z]", waga)
-    sprawdzenie2 = re.match(r"[a-z]", wzrost)
-    if sprawdzenie is None or sprawdzenie2 is None:
-        int_waga = int(waga)
-        int_wzrost = int(wzrost)
-        int_wzrost = int_wzrost/100
-        wynik = round(int_waga/int_wzrost**2, 1)
-        print("Twoje BMI to {}".format(wynik))
-        if wynik >= 50:
-            print("Musisz zastosować dietę!")
-        if wynik <= 10:
-            print("Nawet mój ojciec nie jest taki chudy!")           
-    else:
-        print("Źle podana wartość!")
-    print()
+    try:
+        waga=input("Podaj swoją wagę w kilogramach lub wpisz exit aby wyjść: ")
+        if waga == "exit":
+            print("Do widzenia")
+            break
+        wzrost=input("Podaj swój wzrost w centymetrach: ")
+        if wzrost == "exit":
+            print("Do widzenia")
+            break
+        sprawdzenie = re.match(r"[a-z]", waga)
+        sprawdzenie2 = re.match(r"[a-z]", wzrost)
+        if sprawdzenie is None or sprawdzenie2 is None:
+            int_waga = int(waga)
+            int_wzrost = int(wzrost)
+            int_wzrost = int_wzrost/100
+            wynik = round(int_waga/int_wzrost**2, 1)
+            print("Twoje BMI to {}".format(wynik))
+            if wynik >= 50:
+                print("Musisz zastosować dietę!")
+            if wynik <= 10:
+                print("Nawet mój ojciec nie jest taki chudy!")
+        else:
+            print("Źle podana wartość!")
+        print()
+    except ValueError:
+        print("Błędna wartość!")
     
          
